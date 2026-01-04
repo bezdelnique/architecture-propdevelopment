5aea0503afd2c81dcb052f3c9d83df295f507d35
Vitaly Belenky <v.belenkij@sigma-it.ru>
2026-01-04 17:03:39 +0300

    task5

--- Task5/1. networking.sh
+++ Task5/1. networking.sh
@@ -10,28 +10,28 @@ metadata:
 spec:
   podSelector:
     matchLabels:
-      role: back-end-api
+      role: back-end-api          # Protect this pod
   policyTypes:
-  - Ingress
+    - Ingress
   ingress:
-  - from:
-      - podSelector:
-          matchLabels:
-            app: front-end
+    - from:
+        - podSelector:
+            matchLabels:
+              role: front-end     # Only this can connect
 ---
 apiVersion: networking.k8s.io/v1
 kind: NetworkPolicy
 metadata:
-  name: allow-admin-frontend-to-backend
+  name: allow-admin-frontend-to-admin-backend
 spec:
   podSelector:
     matchLabels:
-      role: admin-back-end-api
+      role: admin-back-end-api    # Protect this pod
   policyTypes:
-  - Ingress
+    - Ingress
   ingress:
-  - from:
-      - podSelector:
-          matchLabels:
-            app: admin-front-end
+    - from:
+        - podSelector:
+            matchLabels:
+              role: admin-front-end  # Only this can connect
 EOF
